6ad01f21b5e594dcd724116eda61b694998e4c3e
Anton <aner-anton@yandex.ru>
2024-06-03 20:43:51 +0300

    fix: vite img build

---
+++ deploy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/AntonLeontev/lukomore.git');
+set('keep_releases', 3);
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+task('build', function () {
+    cd('{{release_path}}');
+    run('npm install');
+    run('npm run build');
+});
+
+// Hosts
+
+host('81.177.139.224')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/lukomore');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
+after('deploy:vendors', 'build');
